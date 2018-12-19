6cf53b4a855d9cbd55be1f2670dbfebf2ae35759
Tomotada Saigusa <nirot1r@g-second.net>
2018-12-19 17:44:54 +0900

    設計の作成

--- design/media.go
+++ design/media.go
@@ -1,22 +1,27 @@
 package design // The convention consists of naming the design
 // package "design"
 import (
-	. "github.com/goadesign/goa/design" // Use . imports to enable the DSL
+	. "github.com/goadesign/goa/design"
 	. "github.com/goadesign/goa/design/apidsl"
 )
 
-// BottleMedia defines the media type used to render bottles.
-var BottleMedia = MediaType("application/vnd.goa.example.bottle+json", func() {
-	Description("A bottle of wine")
-	Attributes(func() { // Attributes define the media type shape.
-		Attribute("id", Integer, "Unique bottle ID")
-		Attribute("href", String, "API href for making requests on the bottle")
-		Attribute("name", String, "Name of wine")
-		Required("id", "href", "name")
-	})
-	View("default", func() { // View defines a rendering of the media type.
-		Attribute("id")   // Media types may have multiple views and must
-		Attribute("href") // have a "default" view.
-		Attribute("name")
-	})
+// LineInformationMediaType is a one of support lines.
+var LineInformationMediaType = MediaType("application/json", func() {
+	attributes := func() {
+		Attribute("ID")
+		Attribute("CorporationName")
+		Attribute("LineName")
+	}
+	Attributes(attributes)
+	View("default", attributes)
+})
+
+// SettingsMediaType is a setting.
+var SettingsMediaType = MediaType("appliaction/json", func() {
+	attributes := func() {
+		Attribute("lines", ArrayOf(String))
+		Attribute("times", ArrayOf(HashOf(String, String)))
+	}
+	Attributes(attributes)
+	View("default", attributes)
 })
--- design/resource.go
+++ design/resource.go
@@ -5,17 +5,87 @@ import (
 	. "github.com/goadesign/goa/design/apidsl"
 )
 
-var _ = Resource("linkcode", func() {
-	BasePath("/linkcode")
+// JWT Components
+var JWT = JWTSecurity("jwt", func() {
+	Header("Authorizaion")
+	Scope("api:access", "API Access")
+})
 
-	Action("create", func() {
-		Description("リンクコード生成")
-		Routing(GET("/:uniqueCode"))
-		Params(func() {
-			Param("uniqueCode", String, "Widget unique code")
+var _ = Resource("jwt", func() {
+	BasePath("/jwt")
+
+	Action("signin", func() {
+		Description("ID/Passペアで認証を行う")
+		Routing(POST("/signin"))
+
+		Payload(func() {
+			Member("userScreenName")
+			Member("password")
 		})
 
-		Response(OK, "text/plain")
+		Response(NoContent, func() {
+			Headers(func() {
+				Header("Authorization", String, "Generated JWT")
+			})
+		})
+		Response(Forbidden)
+	})
+})
+
+var _ = Resource("accounts", func() {
+	BasePath("/accounts")
+
+	Action("createAccount", func() {
+		Description("アカウントの作成を行う")
+		Routing(POST(""))
+
+		Payload(func() {
+			Member("screenName")
+			Member("password")
+		})
+
+		Response(OK)
+		Response(BadRequest)
+	})
+})
+
+var _ = Resource("devices", func() {
+	BasePath("/devices")
+
+	Action("deviceList", func() {
+		Description("デバイス一覧を返す")
+		Routing(GET(""))
+		Security(JWT, func() {
+			Scope("api:access")
+		})
+
+		Response(OK)
+	})
+
+	Action("generateLinkCode", func() {
+		Description("ユニークコードを使用してリンクコードを生成する")
+		Routing(GET("/:uniqueCode"))
+
+		Params(func() {
+			Param("uniqueCode", String, "Device unique code")
+		})
+
+		Response(OK)
+	})
+
+	Action("linkDevice", func() {
+		Description("リンクコードを使用してユニークコードとアカウントを鎖付けする")
+		Routing(PATCH("/:linkCode"))
+		Security(JWT, func() {
+			Scope("api:access")
+		})
+
+		Params(func() {
+			Param("linkCode", String, "LinkCode")
+		})
+
+		Response(OK)
+		Response(Forbidden)
 	})
 })
 
@@ -23,12 +93,50 @@ var _ = Resource("status", func() {
 	BasePath("/status")
 
 	Action("get", func() {
-		Description("ステータス取得")
+		Description("ユニークコードを基にデバイスが本来あるべき状態のデータを取得する")
 		Routing(GET("/:uniqueCode"))
+
 		Params(func() {
-			Param("uniqueCode", String, "Widget unique code")
+			Param("uniqueCode", String, "Device unique code")
 		})
 
-		Response(OK, "text/plain")
+		Response(OK)
+		Response(NotFound)
+	})
+
+	Action("settings", func() {
+		Description("デバイスの設定を定義する")
+		Routing(POST("/:uniqueCode/settings"))
+		Security(JWT, func() {
+			Scope("api:access")
+		})
+
+		Params(func() {
+			Param("uniqueCode", String, "Device unique code")
+		})
+		Payload(func() {
+			Member("userScreenName")
+			Member("password")
+			Member("continueTimes")
+			Member("lines")
+		})
+
+		Response(OK, SettingsMediaType)
+		Response(NotFound)
+	})
+})
+
+var _ = Resource("resources", func() {
+	BasePath("/resources")
+
+	Security(JWT, func() {
+		Scope("api:access")
+	})
+
+	Action("supportLine", func() {
+		Description("対応している路線とその固有IDを返す")
+		Routing(GET("/supports/lines"))
+
+		Response(OK, ArrayOf(LineInformationMediaType))
 	})
 })
